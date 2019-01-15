261a10f98be800a16db2b1f0c42335cd51f33fae
Ismail-ahmadi <ahmadi.ismail92@gmail.com>
2019-01-15 22:43:08 +0700

    modify menu

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,23 +1,23 @@
-// import Flickity from 'flickity';
-// import 'flickity/dist/flickity.min.css';
+import Flickity from 'flickity';
+import 'flickity/dist/flickity.min.css';
 import '../stylesheets/style.scss';
 import * as config from '../../config/config.json';
 
-// const flickity = new Flickity('.gallery', {
-//   contain: true,
-//   // More options are available: (commentout)
-//   // https://flickity.metafizzy.co/options.html (commentout)
-// });
+const flickity = new Flickity('.gallery', {
+  contain: true,
+  // More options are available: (commentout)
+  // https://flickity.metafizzy.co/options.html (commentout)
+});
 
 const burger = document.querySelector('button.burger');
 const burgerActive = 'burger--morph';
 const mobilePanel = document.querySelector('.mobile__panel');
 const mobilePanelActive = 'mobile__panel--open';
 
-// burger.addEventListener('click', () => {
-//   burger.classList.toggle(burgerActive);
-//   mobilePanel.classList.toggle(mobilePanelActive);
-// });
+burger.addEventListener('click', () => {
+  burger.classList.toggle(burgerActive);
+  mobilePanel.classList.toggle(mobilePanelActive);
+});
 
 // import qs from 'qs'; (commentout)
 // import axios from 'axios'; (commentout)
